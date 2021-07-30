f3f9d2703704ba14e4828879c770d6f8599ba48b
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-07-30 13:23:09 +0530

    Add user registeration/login using async

---
+++ block-BNaafp/login-App/routes/users.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/User');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send({ message: 'User Info' });
+});
+
+// Registration 
+
+router.post('/register', async (req, res, next) => {
+  try {
+    var user =  await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Login 
+
+router.post('/login', async (req, res, next) => {
+  var { email, password } = req.body;
+  if(!email || !password) {
+    return res.status(400).json({ error: " email/password requrired" });
+  }
+  try {
+    var user =  await User.findOne({ email });
+    if(!user) {
+      return res.status(400).json({ error: "Email not registered!" })
+    }
+    var result = await user.verifyPassword(password);
+    if(!result) {
+      return res.status(400).json({ error: "Incorrect Password!" })
+    }
+    return res.status(200).json({ message: "User logged in sucessfully!" })
+    
+  } catch (error) {
+    next(error);
+  }
+
+  });
+
+module.exports = router;
